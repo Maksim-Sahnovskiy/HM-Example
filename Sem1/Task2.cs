ec3be5b9a6766772b62bb3d72343c5af6d8ca0b6
Maksim <sakhnovskiy1999@gmail.com>
2022-12-05 17:57:55 +0300

    Решение второй задачи

--- Sem1/Task2.cs
+++ Sem1/Task2.cs
@@ -1,1 +1,20 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите число ");
+string input = Console.ReadLine()!;
+Console.WriteLine("Введите число ");
+string inp = Console.ReadLine()!;
+int A = int.Parse(input);
+int B = int.Parse(inp);
+if (A > B)
+{
+    Console.WriteLine("Наибольшее чило " + A);
+    Console.WriteLine("Наименьшее число" + B);
+}
+else if (B > A)
+{
+    Console.WriteLine("Наибольшее чило " + B);
+    Console.WriteLine("Наименьшее число" + A);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}

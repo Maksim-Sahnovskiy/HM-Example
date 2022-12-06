f3b861ad71b3a5d5b0959d135a22794b0a0fc8a2
Maksim <sakhnovskiy1999@gmail.com>
2022-12-06 18:55:18 +0300

    Решение задач

--- Sem2/Task10.cs
+++ Sem2/Task10.cs
@@ -1,1 +1,7 @@
-Console.WriteLine("Hello World");
+Console.WriteLine("Введите трёхзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int first_number = number / 100;
+int third_number = number % 10;
+int second_number = (number - (first_number * 100) - third_number) / 10;
+
+Console.WriteLine(second_number);
